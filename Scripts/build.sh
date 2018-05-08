9b140072fb5a2cc4f729dfc630d049965019ba26
e-ago <elena.ago@gmail.com>
2018-05-08 06:34:59 -0700

    build.sh scripts changes: 2 makefiles (libs and apps)

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -26,21 +26,43 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# ======== PATH SETUP ========
-export PREFIX="/path/to/gdasync"
-MPI_PATH="/path/to/mpi"
-CUDA_PATH="/path/to/cuda"
+# ======== INPUT PARAMS ========
+BUILD_LIBS=1
+BUILD_APPS=1
+#Build Libraries only
+if [[ $1 -eq 1 ]]; then BUILD_APPS=0; fi
+#Build Apps only
+if [[ $1 -eq 2 ]]; then BUILD_LIBS=0; fi
 
-LIBGDSYNC_PATH="$PREFIX/libgdsync"
-LIBMP_PATH="$PREFIX/libmp"
+# ======== PATH SETUP ========
+export PREFIX="$HOME/gdasync"
+export PREFIX_LIBS="$PREFIX/Libraries"
+export PREFIX_APPS="$PREFIX/Apps"
+
+export MPI_PATH="$HOME/openmpi-2.1.3/build"
+export CUDA_PATH="/usr/local/cuda-9.0"
+
+LIBGDSYNC_PATH="$PREFIX_LIBS/libgdsync"
+LIBMP_PATH="$PREFIX_LIBS/libmp"
+
+# ======== PATH CHECK ========
+[[ -z $PREFIX ]]        && { echo "ERROR: PREFIX env var empy";         		exit 1; }
+[[ ! -d $PREFIX ]]        && { echo "ERROR: PREFIX $PREFIX does not exist";       	exit 1; }
+
+[[ -z $MPI_HOME ]]      && { echo "ERROR: MPI_HOME env var empy";       		exit 1; }
+[[ ! -d $MPI_HOME ]]      && { echo "ERROR: MPI_HOME $MPI_HOME does not exist";   	exit 1; }
+
+[[ -z $CUDA_PATH ]]     && { echo "ERROR: CUDA_PATH env var empy";   	        	exit 1; }
+[[ ! -d $CUDA_PATH ]]     && { echo "ERROR: CUDA_PATH $CUDA_PATH does not exist";	        exit 1; }
+
+
+mkdir -p $PREFIX_LIBS/bin
+mkdir -p $PREFIX_LIBS/include
+mkdir -p $PREFIX_LIBS/lib
+
 
 # ======== MPI ========
-if [ -d $MPI_PATH ]; then
-    export MPI_HOME=$MPI_PATH
-else
-    echo "ERROR: cannot find OpenMPI in $MPI_PATH "
-fi
-
+export MPI_HOME=$MPI_PATH
 echo "MPI_HOME=$MPI_HOME"
 export MPI_NAME=openmpi
 export MPI_BIN=$MPI_HOME/bin
@@ -49,17 +71,11 @@ export MPI_LIB=$MPI_HOME/lib:$MPI_HOME/lib64
 export PATH=$MPI_BIN:$PATH
 
 # ======== CUDA ========
-if [ ! -z "$CUDA" ]; then
-	echo "WARNING: CUDA is already defined ($CUDA), overwriting it..."
-fi
-
-if [ -e $CUDA_PATH ]; then
-	echo "loading $CUDA_PATH environment..."
-	export CUDA=$CUDA_PATH
-	export CUDA_PATH=$CUDA_PATH
-	export CUDA_HOME=$CUDA
-	export PATH=$CUDA/bin:$PATH
-fi
+echo "loading $CUDA_PATH environment..."
+export CUDA=$CUDA_PATH
+export CUDA_PATH=$CUDA_PATH
+export CUDA_HOME=$CUDA
+export PATH=$CUDA/bin:$PATH
 
 CUDADRV=$CUDA
 CUDADRVLIB=$CUDADRV/lib64 #/usr/lib64
@@ -79,33 +95,38 @@ CU_LDFLAGS="$CU_LDFLAGS -L/usr/lib64"
 export CUDADRV
 export CU_CPPFLAGS CU_LDFLAGS
 
-[[ -z $PREFIX ]] 	&& { echo "ERROR: PREFIX env var empy";		exit 1; }
-[[ -z $MPI_HOME ]] 	&& { echo "ERROR: MPI_HOME env var empy";	exit 1; }
-[[ -z $CUDA ]] 		&& { echo "ERROR: CUDA env var empy";		exit 1; }
+PREFIX_TMP=$PREFIX
+export PREFIX=$PREFIX_LIBS
 
-mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/include
-mkdir -p $PREFIX/lib
+# === Building GDAsync libraries
+if [[ $BUILD_LIBS -eq 1 ]]; then
+	# Configuration cleanup
+	rm -rf $PREFIX_LIBS/libgdsync/build
+	rm -rf $PREFIX_LIBS/libmp/build
+	# Libraries cleanup
+	rm -rf $PREFIX_LIBS/lib/*
+	# Binaries cleanup
+	rm -rf $PREFIX_LIBS/bin/*
 
-# Configuration cleanup
-rm -rf $PREFIX/libgdsync/build
-rm -rf $PREFIX/libmp/build
-# Libraries cleanup
-rm -rf $PREFIX/lib/*
-# Binaries cleanup
-rm -rf $PREFIX/bin/*
+	make -f Makefile.libs all || exit 1
+	#export PREFIX=$PREFIX_TMP
+	
+	echo ""
+	echo ""
+	echo "### GDRCopy, LibGDSync and LibMP correctly built ###"
+	echo "# Libraries in $PREFIX_LIBS/lib"
+	echo "# Headers in $PREFIX_LIBS/include"
+	echo "# Binaries in $PREFIX_LIBS/bin"
+	echo "#####################################################"
+	echo ""
 
-make all || exit 1
-
-echo ""
-echo ""
-echo "### GDRCopy, LibGDSync and LibMP correctly built ###"
-echo "# Libraries in $PREFIX/lib"
-echo "# Headers in $PREFIX/include"
-echo "# Binaries in $PREFIX/bin"
-echo "#####################################################"
-echo ""
-echo ""
+fi
 
 # ======== Set LD_LIBRARY_PATH ========
 #export LD_LIBRARY_PATH=$PREFIX/lib:${LD_LIBRARY_PATH}
+
+# === Building GDAsync applications
+if [[ $BUILD_APPS -eq 1 ]]; then
+	echo "Building apps..."
+	make -f Makefile.apps all || exit 1
+fi
