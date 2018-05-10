c357be272b5f431090d7a1eab376ed49adcb2437
e-ago <elena.ago@gmail.com>
2018-05-10 14:17:54 +0200

    initial run scripts

---
+++ Scripts/mapper.sh
@@ -0,0 +1,135 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2011-2018, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#set -x
+
+use_perf=0
+use_nvprof=0
+use_gdb=0
+
+exe=$1
+shift
+params=$*
+
+#Assuming OpenMPI
+lrank=$OMPI_COMM_WORLD_LOCAL_RANK
+
+#Default settings
+USE_GPU=0;
+USE_CPU=0;
+MP_USE_IB_HCA=mlx5_0;
+OMPI_MCA_btl_openib_if_include=mlx5_0;
+
+echo "hostname=${HOSTNAME}"
+echo "lrank=$lrank"
+
+case ${HOSTNAME} in
+	#DGX topology, 1 HCA for 1 GPU
+	*dgx*)
+		if (( $lrank > 4 )); then echo "too many ranks"; exit; fi
+		hlrank=$(($lrank / 2)) # 0,1
+		dlrank=$(($lrank * 2)) # 0,2,4,6
+		#CUDA_VISIBLE_DEVICES=$dlrank
+		USE_GPU=${dlrank}
+		USE_CPU=${hlrank}
+		HCA=mlx5_${lrank}
+		MP_USE_IB_HCA=${HCA}
+		OMPI_MCA_btl_openib_if_include=${HCA}
+	;;
+
+	*brdw0*) CUDA_VISIBLE_DEVICES=3; USE_CPU=0; MP_USE_IB_HCA=mlx5_0;;
+	*brdw1*) CUDA_VISIBLE_DEVICES=0; USE_CPU=0; MP_USE_IB_HCA=mlx5_0;;
+	*ivy2*) CUDA_VISIBLE_DEVICES=0; USE_CPU=0; MP_USE_IB_HCA=mlx5_0;;
+	*ivy3*) CUDA_VISIBLE_DEVICES=0; USE_CPU=0; MP_USE_IB_HCA=mlx5_0;
+	;;
+esac
+
+echo "# ${HOSTNAME}: picking GPU:$CUDA_VISIBLE_DEVICES/$USE_GPU CPU:$USE_CPU HCA:$MP_USE_IB_HCA" >&2
+PATH=$PATH:$PWD
+
+export \
+	CUDA_VISIBLE_DEVICES USE_GPU USE_CPU MP_USE_IB_HCA OMPI_MCA_btl_openib_if_include \
+	\
+	MP_ENABLE_DEBUG 		\
+        MP_ENABLE_WARN 			\
+        GDS_ENABLE_DEBUG 		\
+        MLX5_DEBUG_MASK 		\
+        ENABLE_DEBUG_MSG 		\
+        \
+        MP_DBREC_ON_GPU 		\
+        MP_RX_CQ_ON_GPU 		\
+        MP_TX_CQ_ON_GPU 		\
+        \
+        MP_EVENT_ASYNC 			\
+        MP_GUARD_PROGRESS 		\
+        \
+        GDS_DISABLE_WRITE64           	\
+        GDS_SIMULATE_WRITE64          	\
+        GDS_DISABLE_INLINECOPY        	\
+        GDS_DISABLE_WEAK_CONSISTENCY  	\
+        GDS_DISABLE_MEMBAR            	\
+        GDS_FLUSHER_TYPE		\
+        \
+        MLX5_FREEZE_ON_ERROR_CQE 	\
+        GPU_ENABLE_DEBUG 		\
+        GDRCOPY_ENABLE_LOGGING 		\
+        GDRCOPY_LOG_LEVEL 		\
+        \
+        USE_CALC_SIZE 			\
+        KERNEL_TIME 			\
+        MAX_SIZE 			\
+        USE_GPU_BUFFERS 		\
+        ENABLE_VALIDATION 		\
+        \
+        COMM_USE_COMM 			\
+        COMM_USE_ASYNC_SA 		\
+        COMM_USE_ASYNC_KI 		\
+        \
+        LD_LIBRARY_PATH PATH
+
+set -x
+
+if [ "$use_nvprof" != "0" ]; then
+    now=$(date +%F-%T)
+    inst=${lrank:-bho}
+    #--profile-from-start off
+    nvprof -o /tmp/\%h-${inst}-${now}.nvprof  --replay-mode disabled $exe $params
+    mv -v /tmp/${HOSTNAME}-${inst}-${now}.nvprof .
+elif [ "$use_perf" != "0" ]; then
+    exec perf record -F 99 -o /tmp/$HOSTNAME.prof $exe $params
+elif [ "$use_gdb" != "0" ]; then
+    echo "command options are: $params"
+    sleep 1
+    exec gdb $exe
+elif [ ! -z $USE_CPU ]; then
+    numactl --cpunodebind=${USE_CPU} -l $exe $params
+#    numactl --physcpubind=${USE_CPU} -l $exe $params
+else
+    $exe $params
+# ) 2>&1 |tee bu-${HOSTNAME}.log
+fi
---
+++ Scripts/run_bin.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+source header.sh
+
+[[ ! -e hostfile ]]	&& { echo "ERROR: hostfile missing"; exit 1; }
+[[ ! -e mapper.sh ]]    && { echo "ERROR: mapper.sh missing"; exit 1; }
+
+if [[ $# -lt 2 ]]; then
+    echo "Usage: <num procs> <exec path> <params>"
+    exit 1
+fi
+
+NP=$1
+if [[ $NP -lt 1 ]]; then
+    echo "Illegal procs number: $NP"
+    exit 1
+fi
+
+EXEC=$2
+[[ ! -e $EXEC ]]    && { echo "ERROR: $EXEC not found"; exit 1; }
+shift 2
+PARAMS=$@
+
+#Other parameters
+COMM=0
+COMM_SA=0 #if 1, use SA Model
+COMM_KI=0 #if 1, use KI Model
+TIME=5
+SIZE=131072
+
+#Assuming OpenMPI
+OMPI_params="$OMPI_params --mca btl openib,self"
+OMPI_params="$OMPI_params --mca btl_openib_want_cuda_gdr 1"
+OMPI_params="$OMPI_params --mca btl_openib_warn_default_gid_prefix 0"
+
+export PATH=$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+
+#set -x
+$MPI_HOME/bin/mpirun -verbose  $OMPI_params   	\
+        -x MP_ENABLE_DEBUG=0 			\
+        -x MP_ENABLE_WARN=0 			\
+        -x GDS_ENABLE_DEBUG=0 			\
+        -x MLX5_DEBUG_MASK=0 			\
+        -x ENABLE_DEBUG_MSG=0 			\
+        \
+        -x MP_DBREC_ON_GPU=0 			\
+        -x MP_RX_CQ_ON_GPU=0 			\
+        -x MP_TX_CQ_ON_GPU=0 			\
+        \
+        -x MP_EVENT_ASYNC=0 			\
+        -x MP_GUARD_PROGRESS=0 			\
+        \
+        -x GDS_DISABLE_WRITE64=1           	\
+        -x GDS_SIMULATE_WRITE64=0          	\
+        -x GDS_DISABLE_INLINECOPY=0        	\
+        -x GDS_DISABLE_WEAK_CONSISTENCY=0  	\
+        -x GDS_DISABLE_MEMBAR=0            	\
+        -x GDS_FLUSHER_TYPE=0 		   	\
+        \
+        -x MLX5_FREEZE_ON_ERROR_CQE=0 		\
+        -x GPU_ENABLE_DEBUG=0 			\
+        -x GDRCOPY_ENABLE_LOGGING=0 		\
+        -x GDRCOPY_LOG_LEVEL=1 			\
+        \
+        -x USE_CALC_SIZE=0 			\
+        -x KERNEL_TIME=$TIME 			\
+        -x MAX_SIZE=$SIZE 			\
+        -x USE_GPU_BUFFERS=0 			\
+        -x ENABLE_VALIDATION=0 			\
+        \
+        -x COMM_USE_COMM=$COMM 			\
+        -x COMM_USE_ASYNC_SA=$COMM_SA 		\
+        -x COMM_USE_ASYNC_KI=$COMM_KI 		\
+        \
+        -x LD_LIBRARY_PATH -x PATH 		\
+        --map-by node -np $NP -hostfile hostfile ./mapper.sh $PREFIX_LIBS/bin/$EXEC $PARAMS
+
+# nvprof -o nvprof-kernel.%q{OMPI_COMM_WORLD_RANK}.nvprof
