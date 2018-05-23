aa070449a6c1ec6d8769bac156f5ebf08eb747d6
lab <lab@dgx-1v-12.nvidia.com>
2018-05-23 03:44:32 -0700

    Adding new libgdsync tests and options

--- Scripts/run_libgdsync.sh
+++ Scripts/run_libgdsync.sh
@@ -15,45 +15,93 @@ now=$(date +%F-%T)
 OUT_DIR=$PREFIX_LIBS/out_libgdsync_${now}
 mkdir -p $OUT_DIR
 
-echo "Running gds_sanity..."
+# ================== gds_sanity ==================
+
+echo "Running gds_sanity"
 ofile=$OUT_DIR/gds_sanity.stdout
 efile=$OUT_DIR/gds_sanity.stderr
 ./run_bin.sh 1 $PREFIX_LIBS/bin/gds_sanity 1>$ofile 2>$efile
 
-echo "Running gds_kernel_loopback_latency peersync..."
+echo "Running gds_sanity with GMEM buffers"
+ofile=$OUT_DIR/gds_sanity_gmem.stdout
+efile=$OUT_DIR/gds_sanity_gmem.stderr
+./run_bin.sh 1 $PREFIX_LIBS/bin/gds_sanity -g 1>$ofile 2>$efile
+
+# ============ gds_kernel_loopback_latency ============
+
+echo "Running gds_kernel_loopback_latency peersync"
 ofile=$OUT_DIR/gds_kernel_loopback_latency_peersync.stdout
 efile=$OUT_DIR/gds_kernel_loopback_latency_peersync.stderr
 ./run_bin.sh 1 $PREFIX_LIBS/bin/gds_kernel_loopback_latency 1>$ofile 2>$efile
 
-echo "Running gds_kernel_loopback_latency peersync and descriptors..."
+echo "Running gds_kernel_loopback_latency peersync with GMEM buffers"
+ofile=$OUT_DIR/gds_kernel_loopback_latency_peersync_gmem.stdout
+efile=$OUT_DIR/gds_kernel_loopback_latency_peersync_gmem.stderr
+./run_bin.sh 1 $PREFIX_LIBS/bin/gds_kernel_loopback_latency -E 1>$ofile 2>$efile
+# -------------
+echo "Running gds_kernel_loopback_latency peersync, descriptors"
 ofile=$OUT_DIR/gds_kernel_loopback_latency_peersync_descs.stdout
 efile=$OUT_DIR/gds_kernel_loopback_latency_peersync_descs.stderr
 ./run_bin.sh 1 $PREFIX_LIBS/bin/gds_kernel_loopback_latency -U 1>$ofile 2>$efile
 
-echo "Running gds_kernel_loopback_latency no peersync..."
+echo "Running gds_kernel_loopback_latency peersync, descriptors, GMEM buffers"
+ofile=$OUT_DIR/gds_kernel_loopback_latency_peersync_descs_gmem.stdout
+efile=$OUT_DIR/gds_kernel_loopback_latency_peersync_descs_gmem.stderr
+./run_bin.sh 1 $PREFIX_LIBS/bin/gds_kernel_loopback_latency -U -E 1>$ofile 2>$efile
+# -------------
+echo "Running gds_kernel_loopback_latency !peersync, descriptors"
 ofile=$OUT_DIR/gds_kernel_loopback_latency_no_peersync.stdout
 efile=$OUT_DIR/gds_kernel_loopback_latency_no_peersync.stderr
 ./run_bin.sh 1 $PREFIX_LIBS/bin/gds_kernel_loopback_latency -P -U 1>$ofile 2>$efile
 
-echo "Running gds_kernel_latency.peersync.."
+echo "Running gds_kernel_loopback_latency !peersync, descriptors, GMEM buffers"
+ofile=$OUT_DIR/gds_kernel_loopback_latency_no_peersync_gmem.stdout
+efile=$OUT_DIR/gds_kernel_loopback_latency_no_peersync_gmem.stderr
+./run_bin.sh 1 $PREFIX_LIBS/bin/gds_kernel_loopback_latency -P -U -E 1>$ofile 2>$efile
+
+# ============ gds_kernel_latency ============
+
+echo "Running gds_kernel_latency peersync"
 ofile=$OUT_DIR/gds_kernel_latency.stdout
 efile=$OUT_DIR/gds_kernel_latency.stderr
 ./run_bin.sh 2 $PREFIX_LIBS/bin/gds_kernel_latency 1>$ofile 2>$efile
 
-echo "Running gds_kernel_latency peersync and descriptors..."
+echo "Running gds_kernel_latency peersync, GMEM buffers"
+ofile=$OUT_DIR/gds_kernel_latency_gmem.stdout
+efile=$OUT_DIR/gds_kernel_latency_gmem.stderr
+./run_bin.sh 2 $PREFIX_LIBS/bin/gds_kernel_latency -E 1>$ofile 2>$efile
+# -------------
+echo "Running gds_kernel_latency peersync and descriptors"
 ofile=$OUT_DIR/gds_kernel_latency_peersync_descs.stdout
 efile=$OUT_DIR/gds_kernel_latency_peersync_descs.stderr
 ./run_bin.sh 2 $PREFIX_LIBS/bin/gds_kernel_latency -U 1>$ofile 2>$efile
 
-echo "Running gds_kernel_latency no peersync..."
+echo "Running gds_kernel_latency peersync, descriptors, GMEM buffers"
+ofile=$OUT_DIR/gds_kernel_latency_peersync_descs_gmem.stdout
+efile=$OUT_DIR/gds_kernel_latency_peersync_descs_gmem.stderr
+./run_bin.sh 2 $PREFIX_LIBS/bin/gds_kernel_latency -U -E 1>$ofile 2>$efile
+# -------------
+echo "Running gds_kernel_latency !peersync"
 ofile=$OUT_DIR/gds_kernel_latency_no_peersync.stdout
 efile=$OUT_DIR/gds_kernel_latency_no_peersync.stderr
 ./run_bin.sh 2 $PREFIX_LIBS/bin/gds_kernel_latency -P -U 1>$ofile 2>$efile
 
-echo "Running gds_poll_lat..."
+echo "Running gds_kernel_latency !peersync, GMEM buffers"
+ofile=$OUT_DIR/gds_kernel_latency_no_peersync_gmem.stdout
+efile=$OUT_DIR/gds_kernel_latency_no_peersync_gmem.stderr
+./run_bin.sh 2 $PREFIX_LIBS/bin/gds_kernel_latency -P -U -E 1>$ofile 2>$efile
+
+# ============ gds_poll_lat ============
+
+echo "Running gds_poll_lat"
 ofile=$OUT_DIR/gds_poll_lat.stdout
 efile=$OUT_DIR/gds_poll_lat.stderr
-./run_bin.sh 2 $PREFIX_LIBS/bin/gds_poll_lat 1>$ofile 2>$efile
+./run_bin.sh 1 $PREFIX_LIBS/bin/gds_poll_lat 1>$ofile 2>$efile
+
+echo "Running gds_poll_lat, GMEM buffers"
+ofile=$OUT_DIR/gds_poll_lat_gmem.stdout
+efile=$OUT_DIR/gds_poll_lat_gmem.stderr
+./run_bin.sh 1 $PREFIX_LIBS/bin/gds_poll_lat -E 1>$ofile 2>$efile
 
 echo ""
 echo "Outputs in $OUT_DIR"
